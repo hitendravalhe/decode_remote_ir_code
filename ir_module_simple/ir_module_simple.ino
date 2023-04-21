16123c821347d42249e177eaa9d77fc3af0adbd2
Hitendra S Valhe <112249322+hitendravalhe@users.noreply.github.com>
2023-04-21 13:48:58 +0530

    Update ir_module_simple.ino

--- ir_module_simple/ir_module_simple.ino
+++ ir_module_simple/ir_module_simple.ino
@@ -1,15 +1,39 @@
 #include <IRremote.h>
-IRrecv irrecv(10);
+int IRpin = 11;  // pin for the IR sensor
+int LED = 13;    // LED pin
+IRrecv irrecv(IRpin);
 decode_results results;
-
-void setup() {
+void setup()
+{
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
-  irrecv.enableIRIn();
+  irrecv.enableIRIn();      // Start the receiver
 }
-
 void loop() {
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
+    Serial.println(results.value);
     irrecv.resume();
+
+    // below code helpfull for appication of decodede value
+    //  if(results.value==  16742535 ) // Your ON button value
+    //  {
+    //             digitalWrite(13,LOW);
+    //  }
+    //  else if(results.value == 16732335 ) // Your OFF button value
+    //  {
+    //             digitalWrite(13,HIGH);
+    //  }
+    //  else if(results.value == 16746615 ) // Your OFF button value
+    //  {
+    //             digitalWrite(13,LOW);
+    //  }
+    //  else if(results.value ==  16734375) // Your OFF button value
+    //  {
+    //             digitalWrite(13,HIGH);
+    //  }
+    //  }
+    //  else if(results.value == 16740495) // Your OFF button value
+    //  {
+    //             digitalWrite(13,LOW);
   }
 }
